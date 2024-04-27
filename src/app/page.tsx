99b0b0f6cbe358529296f490848bde911e9a5a5d
Gavin Grant <gavingrant@gmail.com>
2024-04-27 13:55:31 -0700

    refactor: update favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,6 +21,24 @@ export default function Home() {
         <meta property="og:description" content={description} />
         <meta property="og:image" content="/img/markhmendez.jpeg" />
         <meta property="og:image:type" content="image/jpeg" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
 
       <Navbar />
